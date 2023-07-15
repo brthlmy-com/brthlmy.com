94b82483257e448d4ca0ce6feae60a7ac55b87b0
Eric Bartholemy <eric.bartholemy@gmail.com>
2023-07-15 20:50:18 +0200

    fix missing return

--- functions/doorbell/doorbell.js
+++ functions/doorbell/doorbell.js
@@ -9,14 +9,15 @@ const {
 exports.handler = async (event, context) => {
   try {
     const {handler} = await import('@brthlmy/serverless-netlify-doorbell');
-    await handler(event, {
+    const result = await handler(event, {
       googleServiceAccountEmail: GOOGLE_SERVICE_ACCOUNT_EMAIL,
       googlePrivateKey: GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
       spreadsheetId: SPREADSHEET_ID,
       spreadsheetSheetTitle: SPREADSHEET_SHEET_TITLE,
       apexDomain: APEX_DOMAIN,
     });
-  } catch (e) {
-    console.log('debug', e);
+    return result;
+  } catch(e) {
+    console.log('debug',e);
   }
 };
