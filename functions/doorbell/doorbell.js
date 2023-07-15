294e031255136bff00029048c969652e1175497b
Eric Bartholemy <eric.bartholemy@gmail.com>
2023-07-15 17:10:53 +0200

    extracts env and passes to handler as config

--- functions/doorbell/doorbell.js
+++ functions/doorbell/doorbell.js
@@ -1,5 +1,19 @@
+const {
+  GOOGLE_SERVICE_ACCOUNT_EMAIL,
+  GOOGLE_PRIVATE_KEY,
+  SPREADSHEET_ID,
+  SPREADSHEET_SHEET_TITLE,
+  APEX_DOMAIN,
+} = process.env;
+
 exports.handler = async (event, context) => {
-  const { handler } from "@brthlmy/serverless-netlify-doorbell";
-  const result = await handler(event, { ...process.env });
+  const {handler} = await import('@brthlmy/serverless-netlify-doorbell');
+  const result = await handler(event, {
+    googleServiceAccountEmail: GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    googlePrivateKey: GOOGLE_PRIVATE_KEY,
+    spreadsheetId: SPREADSHEET_ID,
+    spreadsheetSheetTitle: SPREADSHEET_SHEET_TITLE,
+    apexDomain: APEX_DOMAIN,
+  });
   return result;
 };
