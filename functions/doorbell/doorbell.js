0d74b469d39d078633690b155c1f95bb282174a3
Eric Bartholemy <eric.bartholemy@gmail.com>
2023-07-15 16:55:45 +0200

    spreads process.env in handler arg

--- functions/doorbell/doorbell.js
+++ functions/doorbell/doorbell.js
@@ -1,5 +1,5 @@
 exports.handler = async (event, context) => {
-  const { handler } from "@brthlmy/serverless-netlify-doorbell"
-  const result = await handler(event, process.env);
+  const { handler } from "@brthlmy/serverless-netlify-doorbell";
+  const result = await handler(event, { ...process.env });
   return result;
 };
